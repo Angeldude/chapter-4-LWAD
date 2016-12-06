1aae704360b1e7bf4e03d09820ace6f80c878170
Angel Vanegas <angelman@gmail.com>
2016-12-05 21:53:05 -0500

    jQuery effects, interactivity with keyboard and mouse c licks

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-var main = function(){
-  "use strict";
+var main = function() {
+    "use strict";
 
-  $('.comment-input button').on('click', function(e){
-    console.log("hi");
-  });
+    $('.comment-input button').on('click', function(e) {
+        var new_comment = $("<p>");
+        if ($('.comment-input input').val() !== "") {
+            new_comment.text($('.comment-input input').val());
+            new_comment.hide();
+            $('.comments').append(new_comment);
+            new_comment.fadeIn();
+            $('.comment-input input').val("");
+        }
+    });
+
+    $('.comment-input input').on('keypress', function(e) {
+        var new_comment = $('<p>');
+        if (e.keyCode === 13) {
+            if ($(this).val() !== "") {
+                new_comment.text($(this).val());
+                new_comment.hide();
+                $('.comments').append(new_comment);
+                new_comment.fadeIn();
+                $(this).val("");
+            }
+        }
+    })
 
 }
 
